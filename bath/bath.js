f428787f2820631e7d6a939dc887763fe22b2f24
“Anna <annaovcharova84@gmail.com>
2022-11-13 21:03:15 -0500

    first commit

---
+++ bath/bath.js
@@ -0,0 +1,35 @@
+const button = document.querySelector("#myButton");
+button.addEventListener("click", function() {
+    const music = document.querySelector("#player").play();
+    document.querySelector("#myVideo").play();
+})
+
+const timer = 40;
+let amountTime = timer*60;
+
+function calculateTime(){
+    const countdown = document.querySelector("#countdown");
+    let minutes = Math.floor(amountTime/60);
+    let seconds = amountTime%60;
+
+    if (seconds<10){
+        seconds = "0" + seconds;
+    }
+
+    countdown.textContent = `${minutes} : ${seconds}`;
+    amountTime--;
+
+    if(amountTime<0) {
+        stopTimer()
+        amountTime = 0;
+        music.pause()
+    }
+    
+    function stopTimer() {
+        clearInterval(timerId)
+    }
+
+}
+
+
+let timerId = setInterval(calculateTime, 1000);
